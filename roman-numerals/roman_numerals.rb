a067d14c535c9b751b89e90c6443a65d3f872382
Northrain-cmd <northrain@icloud.com>
2021-06-05 10:06:05 +0300

    Refactored roman_numerals

--- roman-numerals/roman_numerals.rb
+++ roman-numerals/roman_numerals.rb
@@ -1,36 +1,26 @@
 module RomanNumerals
-  DICT = {
-    1 => 'I',
-    5 => 'V',
-    10 => 'X',
-    50 => 'L',
-    100 => 'C',
-    500 => 'D',
-    1000 => 'M'
-  }.freeze
+  DICT = [
+    [1, 'I'],
+    [4, 'IV'], 
+    [5, 'V'],
+    [9, 'IX'],
+    [10, 'X'],
+    [40, 'XL'],
+    [50, 'L'],
+    [90, 'XC'],
+    [100, 'C'],
+    [400, 'CD'],
+    [500, 'D'],
+    [900, 'CM'],
+    [1000, 'M']
+  ].reverse.freeze
 
   def to_roman
-    ms = self.div(1000)
-    cs = (self % 1000).div(100)
-    xs = (self % 1000 % 500 % 100).div(10)
-    ones = (self % 1000 % 500 % 100 % 50 % 10).div(1)
-    ('M' * ms) + convert_to_roman(cs, 100, 500, 1000) + 
-                 convert_to_roman(xs, 10, 50, 100) + 
-                 convert_to_roman(ones,1, 5, 10)
-  end
-
-  private
-
-  def convert_to_roman(number_of_symbols, number, next_number, next_next_number)
-    mids = number_of_symbols.div(5)
-    result = (DICT[next_number] * mids) + (DICT[number] * (number_of_symbols % 5))
-    case number_of_symbols
-    when 4 
-      result = DICT[number] + DICT[next_number]
-    when 9
-      result = DICT[number] + DICT[next_next_number]
+    x = self
+    DICT.each_with_object('') do |(number, roman), result| 
+      times, x = x.divmod(number)
+      result << roman * times
     end
-    result
   end
 end
 
